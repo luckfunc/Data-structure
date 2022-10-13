7bc6421347b0b5a2f744263682cc4f5f7190d5f5
xdd <45672942@qq.com>
2022-10-13 21:32:10 +0800

    并集代码修改

--- 05_集合/01_封装集合.js
+++ 05_集合/01_封装集合.js
@@ -4,31 +4,31 @@
       this.items = {};
     //方法
     // add方法
-    Set.prototype.add = function(value) {
+    Set.prototype.add = function(key, value) {
       //判断集合当前是否已经包含此元素
-      if(this.has(value)) return false;
+      if(this.has(key)) return false;
       //将元素添加到集合中
-      this.items[value] = value;
+      this.items[key] = value;
       
       return true;
     }
 
     //has方法
-    Set.prototype.has = function(value){
+    Set.prototype.has = function(key){
       //hasOwnProperty判断对象是否含有这个属性
-      return this.items.hasOwnProperty(value);
+      return this.items.hasOwnProperty(key);
     }
 
     //remove方法(集合无重复元素且无序, 不能使用下标值删除元素, )
-    Set.prototype.remove = function(value){
+    Set.prototype.remove = function(key){
       //判断集合是否包含这个元素 包含才删除 不包含返回false
-      if(!this.has(value)){
+      if(!this.has(key)){
         
         return false;
       }
 
       //将元素从属性中删除
-      delete this.items[value];
+      delete this.items[key];
 
       return true
     }
@@ -56,15 +56,20 @@
     //1.创建一个新的集合
     const unionSet = new Set();   
     //将A集合的所有元素全部添加到unionSet
+    let keys = Object.keys(this.items); //获取到元素的所有属性得数组
     let values = this.values(); //获取到元素的所有属性值得数组
-    for (let index = 0; index < values.length; index++) {
+
+    for (let index = 0; index < keys.length; index++) {
           //添加每个元素
-          unionSet.add(values[index]);      
+          unionSet.add(keys[index], values[index]);      
     }   
     // 3.取出集合B中的元素 判断是否需要添加到新集合
+    keys = Object.keys(otherSet.items);
     values = otherSet.values();
     for (let index = 0; index < values.length; index++) {
-      unionSet.add(values[index]);      
+      console.log('keys', keys[index]);
+      console.log('values', values[index]);
+      unionSet.add(keys[index], values[index]);      
     }
     return unionSet;
     }
@@ -90,16 +95,16 @@
   //测试并集
   //创建集合A
   const setA = new Set();
-  setA.add('1');
-  setA.add('2');
-  setA.add('3');
-  setA.add('4');
+  setA.add('num1', 1);
+  setA.add('num2', 2);
+  setA.add('num3', 3);
+  setA.add('num4', 4);
   //创建集合B
   const setB = new Set();
-  setB.add('3');
-  setB.add('4');
-  setB.add('5');
-  setB.add('6');
+  setB.add('num3', 24124);
+  setB.add('num4', 123);
+  setB.add('num5', 5);
+  setB.add('num6', 6);
 
   //获取并集
   const resultObj = setA.union(setB);
