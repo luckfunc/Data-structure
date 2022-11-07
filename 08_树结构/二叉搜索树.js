2188da200c226985624feb2989ad9e0291ff1765
xdd <45672942@qq.com>
2022-11-07 21:59:11 +0800

    递归插入节点实现

--- 08_树结构/二叉搜索树.js
+++ 08_树结构/二叉搜索树.js
@@ -10,5 +10,37 @@
     //属性
       this.root = null;
       //方法
+      //插入方法:对外给用户调用的方法
+      BinarySearchTree.prototype.insert = function(key) {
+        //1.根据Key创建节点
+        const newNode = new Node(key);
+        //2.看是否是根节点
+        if (this.root == null) {
+            this.root = newNode;          
+        }else{
+          //表示有根节点
+          this.insertNode(this.root, newNode);
+        }
+      }
+      //递归插入节点
+      BinarySearchTree.prototype.insertNode = function(node, newNode){
+           if (newNode.key < node.key) {
+            //新节点的key比要比较的节点的key小  向左走
+            
+            //如果左子节点为空
+            if (node.left == null) {
+              node.left = newNode;
+            }else{
+              this.insertNode(node.left, newNode);
+            }
+           }else{
+            //向右查找
+            if(node.right == null){
+              node.right = newNode;
+            }else{
+              this.insertNode(node.right, newNode);
+            }
+           }
+      }
 
   }
