efde975afe563e61ff82c51aabbe1754d5d6908c
xdd <45672942@qq.com>
2022-11-07 21:01:23 +0800

    二叉搜索树的结构

---
+++ 08_树结构/二叉搜索树.js
@@ -0,0 +1,14 @@
+//封装二叉搜索树
+  function BinarySearchTree() {
+    //节点
+    function Node(key) {
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
+
+    //属性
+      this.root = null;
+      //方法
+
+  }
